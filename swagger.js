a0c65e608e789e36467457bcfa1840e552eb34c3
izelomabarak <joseluisim.lds@gmail.com>
2026-01-22 17:10:33 -0600

    a

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title:'Users Api',
         description: 'Users Api'
     },
-    host: 'https://cse-341-project2-6xrp.onrender.com',
+    host: 'cse-341-project2-6xrp.onrender.com',
     schemes: ['https']
 };
 
