5a34a16fd55155f3ba65fad9d43848b11f768338
izelomabarak <joseluisim.lds@gmail.com>
2026-01-22 18:51:28 -0600

    a

--- controllers/validator.js
+++ controllers/validator.js
@@ -1,21 +1,21 @@
 const { body, validationResult } = require('express-validator')
 const userRules = () => {
   return [
-    body('firstName').isLength({ min: 2 }),
-    body('lastName').isLength({ min: 2 }),
-    body('email').isEmail(),
+    body('firstName').isLength({ min: 2 }).withMessage('The name must be minimumt 3 characters'),
+    body('lastName').isLength({ min: 2 }).withMessage('The last name must be minimum 3 characters'),
+    body('email').isEmail().withMessage('The email mus have the email estructure(x.@x.com)'),
   ]
 }
 
 const productRules = () => {
   return [
-    body('name').isLength({ min: 2 }),
-    body('maker').isLength({ min: 2 }),
-    body('country').isLength({ min: 2 }),
-    body('description').isLength({ min: 3 }),
-    body('price').isLength({ min: 1 }),
-    body('stock').isLength({ min: 1 }),
-    body('category').isLength({ min: 1 }),
+    body('name').isLength({ min: 2 }).withMessage('The name of the product must be minimum 2 characters'),
+    body('maker').isLength({ min: 2 }).withMessage('The maker name of the product must be minimum  2 characters'),
+    body('country').isLength({ min: 3 }).withMessage('The country of origin of the product must be minimum  2 characters'),
+    body('description').isLength({ min: 4 }).withMessage('The description most have minimum 2 words'),
+    body('price').isFloat().withMessage('The price most be a number'),
+    body('stock').isInt().withMessage('The stock most be a integer number'),
+    body('category').isLength({ min: 2 }).withMessage('The category of the product must be minimum  2 characters'),
   ]
 }
 
