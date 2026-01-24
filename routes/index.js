a15285cd3ae4dd4c65c2f16f5edc11be2b78fc89
izelomabarak <joseluisim.lds@gmail.com>
2026-01-23 20:44:56 -0600

    a

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ router.use('/', require('./swagger'));
 
 router.get('/', (req,res) => {
     //#swagger.tags=['Hello World']
-    res.send('Hello World');
+    errorHandeler.handleErrors(res.send('Hello World'));
 });
 
 router.use('/users', require('./users'));
