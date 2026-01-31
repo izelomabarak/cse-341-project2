83da0ce81d7b335552478203bb851ee2988653d5
izelomabarak <joseluisim.lds@gmail.com>
2026-01-30 23:47:17 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,37 @@
 const passport = require('passport');
-
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req,res) => {
-    //#swagger.tags=['Hello World']
-    try {
-        res.setHeader('Content-Type', 'application/json');
-        res.send('Hello World');
-    } catch (err) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({error: 'Error', message: err.message,});
-    }
+router.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send('Hello World');
 });
 
 router.use('/users', require('./users'));
 router.use('/products', require('./products'));
 
-router.get ('/auth/github', passport.authenticate('github'), (req, res) => {});
-router.get ('/logout', function(req, res, next) {
-    req.logout (function(err) {
-        if (err) { return next(err); }
-        res.redirect('/')
-    });
+router.get(
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] })
+);
+
+router.get(
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/api-docs'
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
+
+router.get('/logout', (req, res, next) => {
+  req.logout(err => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
 });
 
-module.exports = router; 
+module.exports = router;
