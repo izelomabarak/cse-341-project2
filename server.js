8775c1b1f9f90feed5da05716bdbf7f50ca7c6fd
izelomabarak <joseluisim.lds@gmail.com>
2026-01-30 23:07:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ passport.deserializeUser((user, done) => {
 
 app.get('/', (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')});
 
-app.get('/github/callback', 
+app.get('/auth/github/callback', 
     passport.authenticate('github', {
     failureRedirect: '/api-docs', 
     session: false
